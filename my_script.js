0c9d0b1fef73e5a263af39719b8f2904ad3c4d83
Raphael-Bezerra <raphabzerra15@gmail.com>
2022-05-26 08:48:21 -0300

    aulajs

---
+++ my_script.js
@@ -0,0 +1,8 @@
+//alert('Seja benvindo ao curso JavaScript')
+
+/* 
+        selecionar um elemento no DOM
+        atualizar o valor desse elemento com uma string
+*/
+
+document.getElementById('nome').value = 'Vamos aprender bastante'
